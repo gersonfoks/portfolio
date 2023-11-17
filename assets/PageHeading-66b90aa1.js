9ff2f04a63e6925219fdcf07266d392c2a020b97
gerson <gerson.foks@gmail.com>
2023-11-17 15:37:13 +0100

    switched to gifs

---
+++ assets/PageHeading-66b90aa1.js
@@ -0,0 +1,1 @@
+import{_ as s,o,c as a,d as e,t as r}from"./index-6a8c5de8.js";const c={name:"PageHeading",props:{title:{type:String,required:!0}}},i={class:"hero is-primary is-small"},n={class:"hero-body"},_={class:"title has-text-centered"};function d(l,p,t,h,m,g){return o(),a("section",i,[e("div",n,[e("p",_,r(t.title),1)])])}const u=s(c,[["render",d]]);export{u as P};
